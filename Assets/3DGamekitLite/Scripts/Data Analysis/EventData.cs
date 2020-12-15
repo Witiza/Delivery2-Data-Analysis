4d851b4d59ea117c3410d3533f324ce3164d8b8c
Dani Lorenzo <dlorenzolaguno17@gmail.com>
2020-12-15 17:36:29 +0100

    Update EventData.cs

--- Assets/3DGamekitLite/Scripts/Data Analysis/EventData.cs
+++ Assets/3DGamekitLite/Scripts/Data Analysis/EventData.cs
@@ -1,18 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EventData : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    uint eventID;
+    DateTime timestamp;
+
+    string GetJson()
     {
-        
+        string json = JsonUtility.ToJson(this);
+        return json;
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
+}
+
+public class DeathEvent : EventData
+{
+    Vector3 position;
+    Vector3 eulerAngles;
 }
